e68194cf263412bc21a06afacb64c8b07224153d
prakashlohanathan <121917226+prakashlohanathan@users.noreply.github.com>
2023-04-12 11:57:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function start(){
+    var firstname=document.getElementById("fname").value;
+    console.log(`Firstname:${firstname}`);
+    
+    var middlename=document.getElementById("mname").value;
+    console.log(`Middlename:${middlename}`);
+     
+    let email=document.getElementById("mail").value;
+    console.log(`Email:${email}`);
+
+    let adress=document.getElementById("address").value;
+    console.log(`Addresss:${adress}`);
+
+}
